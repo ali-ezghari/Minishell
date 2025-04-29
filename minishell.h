5f67eff837d454a191779f086d6beed05707b731
Dilor <aezghari90@gmail.com>
2025-04-29 17:42:27 +0100

    Update minishell.h
    Added essential structs for lexical analysis

--- minishell.h
+++ minishell.h
@@ -7,6 +7,52 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+/**
+ * Linked list node representing an environment variable.
+ * Stores the variable's key and value.
+ */
+typedef struct s_env_node
+{
+    char *key;
+    char *value;
+
+    struct s_env_node *next;
+} t_env_node;
+
+/**
+ * Stores the history of previously executed commands.
+ * Uses a dynamic array of strings.
+ */
+typedef struct s_history
+{
+    char **cmds;
+    int count;
+    int capacity;
+} t_history;
+
+typedef enum s_token_type
+{
+    TOKEN_WORD,
+    TOKEN_PIPE,
+    TOKEN_REDIR_IN,
+    TOKEN_REDIR_OUT,
+    TOKEN_REDIR_APPEND,
+    TOKEN_HEREDOC
+} t_token_type;
+
+/**
+ * Represents a single token from the input.
+ * - type: The token's type (e.g., TOKEN_REDIR_OUT for ">")
+ * - value: The string value of the token
+ */
+typedef struct s_token
+{
+    t_token_type type;
+    char *value;
+    struct s_token *next;
+} t_token;
+
+
 char	**input_split(char *input);
 
 #endif
