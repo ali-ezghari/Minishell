ca1043bc1fde83edef15095092a2ae8eafe70509
ali-ezghari <aezghari90@gmail.com>
2025-05-14 10:50:55 +0100

    a simple main to start off with

--- main/shell.c
+++ main/shell.c
@@ -1,6 +1,31 @@
 #include "../includes/minishell.h"
 
+void shell_loop(t_shell *shell)
+{
+    char *line;
+
+    while (1)
+    {
+        line = readline("Minishell>");
+        if (!line || !line[0])
+        {
+            return;
+        }
+        add_history(line);
+        parsing(shell);
+        execution(shell);
+        free_all(&shell->allocator);
+        shell->allocator = NULL;
+        free(line);
+    }
+    clear_history();
+}
+
 int main()
 {
+    t_shell shell;
+
+    shell.allocator = NULL;
+    shell_loop(&shell);
     return (0);
 }
